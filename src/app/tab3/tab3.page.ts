8aa5fce81f2b846e6a8a7934bdd0f0178664152e
DannyVinueza <danny.vinueza@epn.edu.ec>
2023-11-17 15:51:27 -0500

    Implementacion de la camara

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
-
   constructor() {}
-
+    alertButtons = ['Action'];
 }
